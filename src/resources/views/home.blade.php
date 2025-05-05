8f31100c4d0cf4f09033755fa7c3e65ab7c7d240
AnxoPF <a23anxopf@iessanclemente.net>
2025-05-05 18:19:08 +0200

    Blade

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Página de Inicio')
+
+@section('content')
+    <h1>Bienvenido a Laravel</h1>
+    <p>Este es el contenido de la página de inicio.</p>
+@endsection
---
+++ src/resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="{{ url('/') }}">Inicio</a></li>
+            <li><a href="{{ url('/about') }}">Acerca de</a></li>
+            <li><a href="{{ url('/contact') }}">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
